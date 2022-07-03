e4f6e43a6da3663cf624bb7fa6179ca4690c2f91
JashanPuri <jashan2019@iiitkottayam.ac.in>
2022-07-03 17:16:16 +0530

    timestamps added to movie collection

--- models/movie.js
+++ models/movie.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const movieSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please provide a name"],
+const movieSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    overview: {
+      type: String,
+    },
+    poster_path: {
+      type: String,
+    },
+    totalRatings: {
+      type: Number,
+      default: 0,
+    },
+    numberOfReviews: {
+      type: Number,
+      default: 0,
+    },
   },
-  overview: {
-    type: String,
-  },
-  poster_path: {
-    type: String,
-  },
-  totalRatings: {
-    type: Number,
-    default: 0,
-  },
-  numberOfReviews: {
-    type: Number,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Movie", movieSchema);
