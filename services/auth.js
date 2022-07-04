cc318b11da2e45840a502fa38622055eb2cb5e1d
JashanPuri <jashan2019@iiitkottayam.ac.in>
2022-07-04 15:10:16 +0530

    Jwt default expiration set to 30 days

--- services/auth.js
+++ services/auth.js
@@ -37,7 +37,7 @@ const signInUser = async (username, password) => {
     { userId: user._id, username: user.username },
     process.env.JWT_SECRET,
     {
-      expiresIn: process.env.JWT_EXPIRES_IN,
+      expiresIn: process.env.JWT_EXPIRES_IN || "30d",
     }
   );
 };
